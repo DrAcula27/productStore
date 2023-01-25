0033297b26c8d4de8ec76106f50a6745abb50bed
Danielle <danielle@kendallspace.net>
2023-01-25 11:37:14 -0500

    add popup to explain the update page to users

--- public/edit_product/index.js
+++ public/edit_product/index.js
@@ -90,3 +90,14 @@ searchBar.addEventListener("keydown", () => {
 });
 
 // functionality to view shopping cart
+
+// functionality to open popup 0.25 seconds after page load
+window.addEventListener("load", function () {
+  setTimeout(function open() {
+    document.querySelector("#popup").style.display = "block";
+  }, 250);
+});
+
+document.querySelector("#close").addEventListener("click", function () {
+  document.querySelector("#popup").style.display = "none";
+});
